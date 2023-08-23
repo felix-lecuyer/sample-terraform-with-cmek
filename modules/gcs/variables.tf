133871773e66558647c42effca4e51c0f01738cd
Felix Lecuyer <flecuyer@google.com>
2023-08-23 11:22:11 +0200

    Restructure directory to align to terraform best practices

---
+++ modules/gcs/variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  type = string
+  description = "Name of the cluster"
+}
+
+variable "project_id" {
+  description = "Id of the project where to deploy the GKE cluster"
+  type        = string
+}
+
+variable "region" {
+  description = "Region where to deploy the GKE cluster. Default to Frankfurt."
+  default     = "europe-west3"
+  type        = string
+}
+
+variable "kms_key_path" {
+  description = "The Customer Managed Encryption Key used to encrypt the boot disks of the node autoprovisioned by the cluster. This should be of the form projects/[KEY_PROJECT_ID]/locations/[LOCATION]/keyRings/[RING_NAME]/cryptoKeys/[KEY_NAME]"
+  type        = string
+}
